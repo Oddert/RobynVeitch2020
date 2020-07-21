22d03abd191d326bf4308ba73cfe8b1cf27660d4
Robyn <oddert@gmail.com>
2020-07-21 16:44:12 +0100

    styled navbar toggle

--- template-parts/navbar.php
+++ template-parts/navbar.php
@@ -48,7 +48,8 @@
 
 				<nav id="site-navigation" class="main-navigation">
 					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
-						<?php esc_html_e( 'Primary Menu', 'RobynVeitch' ); ?>
+						<i class='fa fa-chevron-left'></i>
+						<i class='fa fa-chevron-right'></i>
 					</button>
 					<?php
 						wp_nav_menu(
