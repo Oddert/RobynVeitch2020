41e415054ca4492f3e6d4c893308fefa320d27d9
Robyn <oddert@gmail.com>
2020-07-31 12:47:21 +0100

    bug fixed links on the new block

--- blocks/block-code-links.php
+++ blocks/block-code-links.php
@@ -14,13 +14,13 @@
 <div class='block-code-links'>
   <div class='wrapper'>
     <?php if ( $repo_link ) : ?>
-      <a class='code-link repo' href='<?php $repo_link ?>'>
+      <a class='code-link repo' href='<?php echo $repo_link ?>'>
         <i class='fa fa-code'></i>
         View Code
       </a>
     <?php endif; ?>
     <?php if ( $demo_link ) : ?>
-      <a class='code-link demo' href='<?php $demo_link ?>'>
+      <a class='code-link demo' href='<?php echo $demo_link ?>'>
         <i class='fa fa-eye'></i>
         Live Demo
       </a>
