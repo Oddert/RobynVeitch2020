075e1f1a1e9e36805bfc8116265e4f4dee6f197f
Robyn Felicity Heather Veitch <robynfhveitch@gnail.com>
2022-05-21 16:21:13 +0100

    added more eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,31 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    node: true
-  },
-  extends: ["eslint:recommended", "wordpress"],
-  parserOptions: {
-    sourceType: "module"
-  },
-  rules: {
-    indent: ["error", "tab"],
-    "linebreak-style": ["off", "unix"],
-    quotes: ["error", "single"],
-    semi: ["error", "never"],
-    "space-in-parens": ["error", "always"],
-    "spaced-comment": ["warn", "always"],
-    "lines-around-comment": "off",
-    "comma-dangle": "off",
-    "no-trailing-spaces": {
-      "skipBlankLines": true
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+	"extends": [
+        "plugin:@wordpress/eslint-plugin/esnext",
+		"eslint:recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
     }
-  }
-};
+}
